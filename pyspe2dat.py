2ecdd7cb71ee7ab0015111b556bf837ceca4178e
Christian Rickert <rc.login@icloud.com>
2021-05-29 01:01:13 -0600

    updated email address

--- pyspe2dat.py
+++ pyspe2dat.py
@@ -3,7 +3,7 @@
 
 '''
 pyspe2dat, Extracts data from Magnettech binary files.
-Copyright (C) 2011  Christian Rickert <mail@crickert.de>
+Copyright (C) 2011  Christian Rickert <rc.email@icloud.com>
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
